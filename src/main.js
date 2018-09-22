03a2dee464d25c8c58c2f63734e8a74fc273975a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-09-22 18:26:52 +0200

    chore: Add viser and iview instances

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from "vue";
+import iView from "iview";
+import Viser from "viser-vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import apolloProvider from "./apollo";
 import i18n from "./locale";
 import "./registerServiceWorker";
+
+Vue.use(iView, {
+  transfer: true
+});
+
+Vue.use(Viser);
 
 Vue.config.productionTip = false;
 
