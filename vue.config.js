ad0c72021fd8138308000ccb3a087faf95c58ac4
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-09-22 15:43:59 +0200

    chore: Define *.gql loader and alias

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
+const path = require("path");
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src")
+      }
+    }
+  },
+
+  chainWebpack: config => {
+    config.module
+      .rule("graphql")
+      .test(/\.gql$/)
+      .use("graphql-tag/loader")
+      .loader("graphql-tag/loader")
+      .end();
+  }
 };
