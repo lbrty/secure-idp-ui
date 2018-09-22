5836eed7555f276d825db4c4d5722c44568e3708
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-09-22 18:16:11 +0200

    chore: Configure max length

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,16 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
 
   rules: {
+    printWidth: 120,
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "max-len": ["error", 120, 2, {
+      ignoreUrls: true,
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: false,
+      ignoreTemplateLiterals: false,
+    }],
   },
 
   parserOptions: {
