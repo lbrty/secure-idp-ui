5b559756609646af59cb71a14460bef01978e2e6
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-09-22 16:10:31 +0200

    chore: Prepare assets

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,9 @@
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<!-- Keep the order of imports and styles -->
+<style>
+@import "./assets/css/balloon.css";
+@import "./assets/css/animate.css";
+@import "../node_modules/iview/dist/styles/iview.css";
 </style>
