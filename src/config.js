1d17d9a54693e4aa4bd50d4dcd3baaa7809c2102
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-09-22 15:38:44 +0200

    chore: Add application configuration file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT;
+
+// Environment flags
+export const IS_DEV = process.env.NODE_ENV === "development";
+
+// 20 MB
+export const MAX_FILE_SIZE = 20971520;
