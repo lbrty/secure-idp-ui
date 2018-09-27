18552d8e81bcbc41e45ebd5c2df0c6d8d53739ba
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-09-27 19:51:02 +0200

    Update eslint rules for no-unused-vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,13 +10,19 @@ module.exports = {
     printWidth: 120,
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "max-len": ["error", 120, 2, {
-      ignoreUrls: true,
-      ignoreComments: false,
-      ignoreRegExpLiterals: true,
-      ignoreStrings: false,
-      ignoreTemplateLiterals: false,
-    }],
+    "no-unused-vars": ["error", { args: "none" }],
+    "max-len": [
+      "error",
+      120,
+      2,
+      {
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: false,
+        ignoreTemplateLiterals: false
+      }
+    ]
   },
 
   parserOptions: {
