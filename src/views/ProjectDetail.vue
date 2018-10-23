b3accc3cea27d9b174c82234f148af6abe9ab659
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-10-23 18:15:33 +0200

    chore: add initial code in project detail

---
+++ src/views/ProjectDetail.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="project-detail">
+    TODO: master detail => router-view
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectDetail",
+  props: {
+    projectId: String
+  }
+};
+</script>
